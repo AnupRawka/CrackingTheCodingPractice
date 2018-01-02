f0a4652194863a40205cf21e1f1bb32044fdf518
AnupRawka <anuprawka@gmail.com>
2018-01-02 13:23:49 -0500

    Update FindAnagrams.scala

--- FindAnagrams.scala
+++ FindAnagrams.scala
@@ -1,35 +1,38 @@
-object Solution {
+// To execute Scala code, please define an object named Solution that extends App
 
- def main(args: Array[String]): Unit = {
-  println(findAnagrams("Loop", List("Pool","oolp","oolpo","lopo","polo")).mkString(" , "))
- }
-
-def getStrMap(str: String): mutable.Map[Char, Int]= {
-  val map = mutable.HashMap[Char, Int]()
-  str.toCharArray.foreach { ch =>
-    map.get(ch) match {
-      case None => map.put(ch, 1)
-      case _ => map.put(ch, map.get(ch).get + 1)
+object Solution extends App {
+  
+  def  getMap(str: String): scala.collection.mutable.Map[Char, Int]={
+    var map=scala.collection.mutable.Map[Char, Int]()
+    str.toLowerCase.toCharArray.foreach{ch =>
+      map.get(ch) match{
+        case None => map.put(ch,1)
+        case Some(x) => map.put(ch, x+1)
+      }
     }
+    map
   }
-  map
-}
-
-  def compareMap(chMap1: mutable.Map[Char, Int], chMap2: mutable.Map[Char, Int]): Boolean={
-    chMap1.keySet.foreach{
-      ch => if (chMap1.get(ch).get != chMap2.get(ch).get) return false
+  
+  def compareMap(map1: scala.collection.mutable.Map[Char, Int], map2: scala.collection.mutable.Map[Char, Int]): Boolean={
+    val keySet=map2.keySet
+    map1.keySet.foreach{
+      key =>
+  map2.get(key) match {
+  case None => return false
+  case Some(x) => if (x != map1.get(key).get) return false
+  }
     }
     true
   }
-  def findAnagrams(str: String, testList: List[String]): ListBuffer[String] ={
-    val list =  new ListBuffer[String]()
-    testList.foreach{st=>
-        if (st.length ==str.length){
-          if (compareMap(getStrMap(str.toLowerCase), getStrMap(st.toLowerCase))) list+=st
-        }
-else println(s"${st} and $str does not have same length")
-      }
-    list
-    }
-    }
-//The worst case complexity of this algorithm is O(3N)
+  
+  
+  def isAnagram(str1: String, str2 : String) :Boolean={
+    compareMap(getMap(str1),getMap(str2))
+  }
+  
+  val testStr="pool"
+  val testList=Array("loop","dump","polo","lolo")
+  testList.foreach{str =>
+  if (!isAnagram(testStr,str)) println(s"Not anagrams $testStr   and   $str")
+  }
+}
